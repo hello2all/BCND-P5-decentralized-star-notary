a8d5ea7dcd4d7cd5b72ff8ceca8435e304795781
hello2all <hello2tly@gmail.com>
2018-11-06 03:23:47 +0800

    add deployment to infura

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -12,14 +12,25 @@
  *   },
  */
 
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = "diet airport token debris vital amateur eight receive deliver fashion hawk hover";
+// address: 0x23d4993ddb6041d0581166bdc7d34ae7bb71c755
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  networks: {
+  networks: { 
     development: {
-      host: "localhost",
+      host: '127.0.0.1',
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*"
+    }, 
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/22e7b7a1d14a425080ee5a8c54b5b067') 
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   }
 };
